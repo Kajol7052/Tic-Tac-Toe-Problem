06112079c0e8956a54491873008a706740653896
“Kajol7052” <kajol7052@gmail.com>
2020-12-14 11:57:02 +0530

    Added : Creating a Empty Game Board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,3 +2,18 @@
 
 echo "-------------------------------------------------WELCOME TO TIC TAC TOE GAME------------------------------------------"
 
+ROW_NUMBER=3
+COLUMN_NUMBER=3
+
+#UC-1 Creating a game board
+function createGameboard() {
+        for (( row=0;row<$ROW_NUMBER;row++ ))
+        do
+                for (( col=0;col<$COLUMN_NUMBER;col++ ))
+                        do
+                                board[$row,$col]=" "
+                        done
+        done
+}
+
+createGameboard
